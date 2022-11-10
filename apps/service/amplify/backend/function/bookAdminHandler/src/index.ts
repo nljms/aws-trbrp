97a24d1a3e02f8641a944ec8c98c4e4c418293fc
Neil Monzales <neiljames97@gmail.com>
2022-11-10 22:19:36 +0800

    Chore: Convert services to typescript

---
+++ apps/service/amplify/backend/function/bookAdminHandler/src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+export const handler = async (event: any) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`)
+  return {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+    },
+    body: JSON.stringify('Hello from Lambda!'),
+  }
+}
